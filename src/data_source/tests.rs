361e21937a930cc0329526f8cae0da1eb85f8091
zrefalogia <vuongquocphongv@gmail.com>
2023-05-30 15:11:06 +0700

    feat(repo): add name, username, email invalid errors

--- src/data_source/tests.rs
+++ src/data_source/tests.rs
@@ -43,3 +43,45 @@ fn test_email_is_not_found() {
     let err = UserDataSourceError::EmailNotFound(email.clone());
     assert_eq!(format!("{}", err), format!("Email {:?} not found", email));
 }
+
+#[test]
+fn test_email_invalid() {
+    use super::user_data_source_error::UserDataSourceError;
+
+    let email1 = String::from("email");
+    let err1 = UserDataSourceError::InvalidEmail(email1.clone());
+
+    let email2 = String::from("");
+    let err2 = UserDataSourceError::InvalidEmail(email2.clone());
+
+    assert_eq!(format!("{}", err1), format!("Email {:?} is invalid", email1));
+    assert_eq!(format!("{}", err2), format!("Email cannot be empty"));
+}
+
+#[test]
+fn test_username_invalid() {
+    use super::user_data_source_error::UserDataSourceError;
+
+    let username1 = String::from("username");
+    let err1 = UserDataSourceError::InvalidUsername(username1.clone());
+
+    let username2 = String::from("");
+    let err2 = UserDataSourceError::InvalidUsername(username2.clone());
+
+    assert_eq!(format!("{}", err1), format!("Username {:?} is invalid", username1));
+    assert_eq!(format!("{}", err2), format!("Username cannot be empty"));
+}
+
+#[test]
+fn test_name_invalid() {
+    use super::user_data_source_error::UserDataSourceError;
+
+    let name1 = String::from("name");
+    let err1 = UserDataSourceError::InvalidNameField(name1.clone());
+
+    let name2 = String::from("");
+    let err2 = UserDataSourceError::InvalidNameField(name2.clone());
+
+    assert_eq!(format!("{}", err1), format!("Name {:?} is invalid", name1));
+    assert_eq!(format!("{}", err2), format!("Name cannot be empty"));
+}
--- src/data_source/user_data_source_error.rs
+++ src/data_source/user_data_source_error.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use mongodb::bson::Uuid;
 
+#[non_exhaustive]
 pub enum UserDataSourceError {
     /// Error when the uid is not found
     UuidNotFound(Uuid),
@@ -16,6 +17,24 @@ pub enum UserDataSourceError {
 
     /// Error when the email is already taken.
     EmailTaken(String),
+
+    /// Error when the user name is empty.
+    EmptyUsername,
+    
+    /// Error when the email is empty.
+    EmptyEmail,
+    
+    /// Error when the name is empty.
+    EmptyName,
+
+    /// Error when the username is invalid.
+    InvalidUsername(String),
+
+    /// Error when the email is invalid.
+    InvalidEmail(String),
+
+    /// Error when the name field provided is invalid.
+    InvalidNameField(String),
 }
 
 impl fmt::Display for UserDataSourceError {
@@ -44,6 +63,52 @@ impl fmt::Display for UserDataSourceError {
             // Display message for email taken
             UserDataSourceError::EmailTaken(email) => {
                 write!(f, "Email {:?} already taken", email)
+            },
+
+            // Display message for empty username
+            UserDataSourceError::EmptyUsername => {
+                write!(f, "Username cannot be empty")
+            },
+
+            // Display message for empty email
+            UserDataSourceError::EmptyEmail => {
+                write!(f, "Email cannot be empty")
+            },
+
+            // Display message for empty name
+            UserDataSourceError::EmptyName => {
+                write!(f, "Name cannot be empty")
+            },
+
+            // Display message for invalid username
+            UserDataSourceError::InvalidUsername(username) => {
+                if username.len() == 0 {
+                    UserDataSourceError::EmptyUsername.fmt(f)
+                }
+                else {
+                    write!(f, "Username {:?} is invalid", username)
+                }
+
+            },
+
+            // Display message for invalid email
+            UserDataSourceError::InvalidEmail(email) => {
+                if email.len() == 0 {
+                    UserDataSourceError::EmptyEmail.fmt(f)
+                }
+                else {
+                    write!(f, "Email {:?} is invalid", email)
+                }
+            },
+
+            // Display message for invalid name
+            UserDataSourceError::InvalidNameField(name) => {
+                if name.len() == 0 {
+                    UserDataSourceError::EmptyName.fmt(f)
+                }
+                else {
+                    write!(f, "Name {:?} is invalid", name)
+                }
             },
         }
     }
