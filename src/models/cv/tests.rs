fb51196a71eb9325aeced0b861360b2958660fd6
produy <nguyentrinhduy2003@gmail.com>
2023-06-01 14:10:06 +0700

    test(repo): add tests for cv-builder

--- src/models/cv/tests.rs
+++ src/models/cv/tests.rs
@@ -1,19 +1,51 @@
 use crate::models::cv::create_cv_input::CreateCVInputBuilder;
-
 #[test]
-fn test_create_cv_input(){
+fn test_create_cv_input_author_id(){
     use mongodb::bson::Uuid;
     let uuid = Uuid::new();
     let test_cv_input = CreateCVInputBuilder::new(
         uuid,
         "title".to_string(),
-        vec!["tag1".to_string()],
     )
-    .with_description("description".to_string())
-    .with_tags("tag2".to_string())
     .build();
     assert_eq!(test_cv_input.author_id, uuid);
-    assert_eq!(test_cv_input.title, "title".to_string());
-    assert_eq!(test_cv_input.description, Some("description".to_string()));
-    assert_eq!(test_cv_input.tags, vec!["tag1".to_string(), "tag2".to_string()]);
 }
+
+#[test]
+fn test_create_cv_input_title(){
+    use mongodb::bson::Uuid;
+    let uuid = Uuid::new();
+    let test_cv_input = CreateCVInputBuilder::new(
+        uuid,
+        "title".to_string(),
+    )
+    .build();
+    assert_eq!(test_cv_input.title, "title".to_string());
+}
+
+#[test]
+fn test_create_cv_input_description(){
+    use mongodb::bson::Uuid;
+    let uuid = Uuid::new();
+    let test_cv_input = CreateCVInputBuilder::new(
+        uuid,
+        "title".to_string(),
+    )
+    .with_description("description".to_string())
+    .build();
+    assert_eq!(test_cv_input.description, Some("description".to_string()));
+}
+
+#[test]
+fn test_create_cv_input_tag(){
+    use mongodb::bson::Uuid;
+    let uuid = Uuid::new();
+    let test_cv_input = CreateCVInputBuilder::new(
+        uuid,
+        "title".to_string(),
+    )
+    .with_tag("tag".to_string())
+    .build();
+    assert_eq!(test_cv_input.tags, vec!["tag".to_string()]);
+}
+
