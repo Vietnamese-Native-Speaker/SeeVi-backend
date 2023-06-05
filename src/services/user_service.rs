760a53ad116ae9a13f19984a6b632b5f93ae12e0
PhucChuHoang <phucchu390@gmail.com>
2023-06-05 14:26:57 +0700

    feat(service): add user service definition

---
+++ src/services/user_service.rs
@@ -0,0 +1,64 @@
+pub struct UserService {
+    
+}
+
+impl UserService {
+    pub async fn registraion(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn authentication(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    //Forget password = change password
+    pub async fn change_password(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_email(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_username(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_name(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_country(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_skills(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn add_cv(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn remove_cv(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn add_email(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn remove_email(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_about(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_avatar(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn change_cover_photo(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn add_friend(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn remove_friend(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn add_education(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+    pub async fn remove_education(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+        unimplemented!()
+    }
+}
