cd4f5e8ed3515c85ce8675b6b1169f3c90103527
ltp1405 <lamtungphan1405@gmail.com>
2023-06-08 21:34:17 +0700

    feat(models): update user model

--- src/models/users/user.rs
+++ src/models/users/user.rs
@@ -2,7 +2,7 @@ use async_graphql::{Enum, SimpleObject};
 use mongodb::bson::Uuid;
 use serde::{Deserialize, Serialize};
 
-use crate::models::education::Education;
+use crate::models::{education::Education, ResourceIdentifier};
 
 #[non_exhaustive]
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Enum, PartialEq, Eq)]
@@ -24,10 +24,13 @@ pub struct User {
     pub primary_email: String,
     pub other_mails: Vec<String>,
     pub about: Option<String>,
-    pub avatar: Option<Uuid>,
-    pub cover_photo: Option<Uuid>,
+    pub avatar: Option<ResourceIdentifier>,
+    pub cover_photo: Option<ResourceIdentifier>,
     pub friends_list: Vec<Uuid>,
     pub education: Vec<Education>,
     pub rating: Option<f64>,
     pub level: Option<Level>,
+    pub shared_cvs: Vec<Uuid>,
+    pub saved_cvs: Vec<Uuid>,
+    pub liked_cvs: Vec<Uuid>,
 }
