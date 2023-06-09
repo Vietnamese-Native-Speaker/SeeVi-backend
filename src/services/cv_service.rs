c2550d44029b7330a8a5d5a2aa34db313324f73c
PhucChuHoang <phucchu390@gmail.com>
2023-06-09 16:22:46 +0700

    feat(repo): add required param for cv services

--- src/services/cv_service.rs
+++ src/services/cv_service.rs
@@ -3,43 +3,25 @@ pub struct CVService {
 }
 
 impl CVService {
-    pub async fn change_title(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn change_owner_name(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn change_cv_id(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn change_published(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn change_time_published(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn change_image(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn change_title(&mut self, title: String) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
     pub async fn change_item_interactions(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
-    pub async fn change_content(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn change_description(&mut self, description: String) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
-    pub async fn change_hightlight(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn add_tags(&mut self, tag: String) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
-    pub async fn add_tags(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn remove_tags(&mut self, tag: String) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
-    pub async fn remove_tags(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn add_suggested_cv_id(&mut self, id: Uuid) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
-    pub async fn add_suggested_cv_id(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
-    }
-    pub async fn remove_suggested_cv_id(&mut self) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
+    pub async fn remove_suggested_cv_id(&mut self, id: Uuid) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
     }
 }
