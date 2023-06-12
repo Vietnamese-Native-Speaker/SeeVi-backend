a7cf6106a76513ff004b771c0d1bbbd87dcc287f
ltp1405 <lamtungphan1405@gmail.com>
2023-06-12 19:09:50 +0700

    fix(models): remove unnecessary fields

--- src/models/users/create_user_input.rs
+++ src/models/users/create_user_input.rs
@@ -13,17 +13,14 @@ pub struct CreateUserInput {
     pub first_name: String,
     pub last_name: String,
     pub country: Option<String>,
-    pub skills: Option<String>,
-    #[builder(setter(custom), field(type = "Vec<Uuid>"))]
-    pub cv: Vec<Uuid>,
+    #[builder(setter(custom), field(type = "Vec<String>"))]
+    pub skills: Vec<String>,
     pub primary_email: String,
     #[builder(setter(custom), field(type = "Vec<String>"))]
     pub other_mails: Vec<String>,
     pub about: Option<String>,
     pub avatar: Option<Uuid>,
     pub cover_photo: Option<Uuid>,
-    #[builder(setter(custom), field(type = "Vec<Uuid>"))]
-    pub friends_list: Vec<Uuid>,
     #[builder(setter(custom), field(type = "Vec<Education>"))]
     pub education: Vec<Education>,
 }
@@ -35,23 +32,17 @@ impl CreateUserInput {
 }
 
 impl CreateUserInputBuilder {
-    pub fn with_cv(mut self, cv: Uuid) -> Self {
-        self.cv.push(cv);
-        self
-    }
-
     pub fn with_other_mail(mut self, other_mails: impl Into<String>) -> Self {
         self.other_mails.push(other_mails.into());
         self
     }
-
-    pub fn with_friend(mut self, friend: Uuid) -> Self {
-        self.friends_list.push(friend);
+    pub fn with_education(mut self, education: Education) -> Self {
+        self.education.push(education);
         self
     }
 
-    pub fn with_education(mut self, education: Education) -> Self {
-        self.education.push(education);
+    pub fn with_skill<T: Into<String>>(mut self, skill: T) -> Self {
+        self.skills.push(skill.into());
         self
     }
 }
--- src/models/users/tests.rs
+++ src/models/users/tests.rs
@@ -1,7 +1,62 @@
+use mongodb::bson::Uuid;
+
 use crate::models::{
     education::Education,
     users::{create_user_input::CreateUserInputBuilder, update_user_input::UpdateUserInputBuilder},
 };
+
+use super::User;
+
+#[test]
+fn test_create_user_input_to_user() {
+    let uuid = Uuid::new();
+    let test_user_input = CreateUserInputBuilder::default()
+        .with_username("username")
+        .with_first_name("first_name")
+        .with_last_name("last_name")
+        .with_country("country")
+        .with_skill("skill")
+        .with_primary_email("primary_email")
+        .with_other_mail("other_mails")
+        .with_other_mail("other_mails2")
+        .with_education(Education {
+            institution: "University of Example 1".to_string(),
+            course: Some("Computer Science".to_string()),
+            degree: Some("Bachelor's Degree".to_string()),
+        })
+        .with_education(Education {
+            institution: "University of Example 2".to_string(),
+            course: Some("Computer Science".to_string()),
+            degree: Some("Bachelor's Degree".to_string()),
+        })
+        .with_about("about".to_string())
+        .with_avatar(uuid)
+        .with_cover_photo(uuid)
+        .build()
+        .unwrap();
+
+    let user = User {
+        user_id: Uuid::new(),
+        username: test_user_input.username,
+        first_name: test_user_input.first_name,
+        last_name: test_user_input.last_name,
+        country: test_user_input.country,
+        skills: test_user_input.skills,
+        primary_email: test_user_input.primary_email,
+        other_mails: test_user_input.other_mails,
+        about: test_user_input.about,
+        education: test_user_input.education,
+        avatar: test_user_input.avatar,
+        cover_photo: test_user_input.cover_photo,
+        rating: panic!(),
+        level: panic!(),
+        shared_cvs: Vec::default(),
+        saved_cvs: Vec::default(),
+        liked_cvs: Vec::default(),
+        friends_list: Vec::default(),
+        cv: Vec::default(),
+    };
+}
 
 #[test]
 fn test_create_user_input_builder() {
@@ -14,14 +69,9 @@ fn test_create_user_input_builder() {
         .with_first_name("first_name")
         .with_last_name("last_name")
         .with_country("country")
-        .with_skills("skills")
         .with_primary_email("primary_email")
-        .with_cv(uuid)
-        .with_cv(uuid2)
         .with_other_mail("other_mails")
         .with_other_mail("other_mails2")
-        .with_friend(uuid)
-        .with_friend(uuid2)
         .with_education(Education {
             institution: "University of Example 1".to_string(),
             course: Some("Computer Science".to_string()),
@@ -42,8 +92,6 @@ fn test_create_user_input_builder() {
     assert_eq!(test_user_input.first_name, "first_name".to_string());
     assert_eq!(test_user_input.last_name, "last_name".to_string());
     assert_eq!(test_user_input.country, Some("country".to_string()));
-    assert_eq!(test_user_input.skills, Some("skills".to_string()));
-    assert_eq!(test_user_input.cv, vec![uuid, uuid2]);
     assert_eq!(test_user_input.primary_email, "primary_email".to_string());
     assert_eq!(
         test_user_input.other_mails,
@@ -52,7 +100,6 @@ fn test_create_user_input_builder() {
     assert_eq!(test_user_input.about, Some("about".to_string()));
     assert_eq!(test_user_input.avatar, Some(uuid));
     assert_eq!(test_user_input.cover_photo, Some(uuid));
-    assert_eq!(test_user_input.friends_list, vec![uuid, uuid2]);
     assert_eq!(
         test_user_input.education,
         vec![
@@ -107,16 +154,3 @@ fn test_update_user_input_builder() {
         }])
     );
 }
-
-
-
-
-    
-    
-            
-    
-        
-    
-           
-    
-
