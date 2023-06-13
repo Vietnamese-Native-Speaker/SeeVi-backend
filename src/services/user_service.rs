58893a3e548d3438967ab03cb61c6e7b995f6670
PhucChuHoang <phucchu390@gmail.com>
2023-06-13 16:50:01 +0700

    feat(service): implement user register

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -2,7 +2,47 @@ pub struct UserService;
 
 impl UserService {
     pub async fn register(database: &mut impl UserDataSource, user_input: CreateUserInput) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
-        unimplemented!()
+        fn check_invalid_characters(s: &str) -> bool {
+            if s.len() == 0 {
+                return true;
+            }
+            let invalid_chars = [
+                ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '[', ']', '{', '}', '|', '\\',
+                '/', '<', '>', '?', ';', ':', '"', '\'', ',', '.'
+            ];
+            for ch in username.chars() {
+                if invalid_chars.contains(&ch) {
+                    return true;
+                }
+            }
+            false
+        }
+
+        if(check_invalid_characters(user_input.username.as_str())) {
+            return Err(Box::new(UserError::InvalidUsername(user_input.username)))
+        }
+        if(check_invalid_characters(user_input.email.as_str())) {
+            return Err(Box::new(UserError::InvalidEmail(user_input.email)))
+        }
+        if(check_invalid_characters(user_input.first_name.as_str())) {
+            return Err(Box::new(UserError::InvalidNameField(user_input.first_name)))
+        }
+        if(check_invalid_characters(user_input.last_name.as_str())) {
+            return Err(Box::new(UserError::InvalidNameField(user_input.last_name)))
+        }
+
+        let username = user_input.username;
+        if(database.find_user_by_username(username.clone()).await.is_ok()) {
+            return Err(Box::new(UserError::UsernameTaken(username)))
+        }
+        let email = user_input.email;
+        if(database.find_user_by_email(email.clone()).await.is_ok()) {
+            return Err(Box::new(UserError::EmailTaken(email)))
+        }
+        if(database.create_user(user_input).await.is_ok()) {
+            return Ok(Box::new("User created successfully"));
+        }
+        Err(Box::new("User creation failed"));
     }
     pub async fn authenticate(database: &mut impl UserDataSource, username: Option<String>, email: Option<String>, password: String) -> Result<User, dyn Box<impl Debug + Clone + Copy>> {
         unimplemented!()
