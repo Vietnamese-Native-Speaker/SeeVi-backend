3d63d8f216b224c08c93ef48559f6f314fda6a90
zrefalogia <vuongquocphongv@gmail.com>
2023-06-13 21:22:54 +0700

    refactor(service): change the return type of new function

--- src/services/storage_service/storage_service.rs
+++ src/services/storage_service/storage_service.rs
@@ -2,25 +2,32 @@ use google_cloud_auth::credentials::CredentialsFile;
 use google_cloud_storage::{client::{ClientConfig, Client}, sign::{SignedURLOptions, SignedURLMethod}};
 use google_cloud_default::WithAuthExt;
 use google_cloud_storage::sign::SignedURLError;
+use google_cloud_auth::error::Error;
 
 use mongodb::bson::Uuid;
-#[allow(dead_code)]
 
 pub struct StorageServer {
     client: Client,
 }
 
 impl StorageServer {
-    pub async fn new(credentials_path: String) -> tide::Result<Self> {
-        let cred = CredentialsFile::new_from_file(credentials_path).await.unwrap();
-        let config = ClientConfig::default().with_credentials(cred).await.unwrap();
-        let client = Client::new(config);
-        Ok(StorageServer{client})
+    pub async fn new(credentials_path: String) -> Result<StorageServer, Error> {
+        let cred = CredentialsFile::new_from_file(credentials_path).await;
+        match cred {
+            Ok(cred) => {
+                let config = ClientConfig::default().with_credentials(cred).await.unwrap();
+                let client = Client::new(config);
+                Ok(StorageServer {
+                    client,
+                })
+            }
+            Err(e) => panic!("Error: {}", e),
+        }
     }
 
     pub async fn url_for_get(&self, user_id: Uuid, file_id: Uuid) -> Result<String, SignedURLError> {
-        let bucket = user_id.to_string();
-        let object = file_id.to_string();
+        let bucket = String::from("crispy-garbanzo");
+        let object = String::from(user_id.to_string() + "/" + &file_id.to_string());
         let url = self.client
             .signed_url(&bucket, &object, None, None, SignedURLOptions{
                 method: SignedURLMethod::GET,
@@ -34,8 +41,8 @@ impl StorageServer {
     }
 
     pub async fn url_for_put(&self, user_id: Uuid, file_id: Uuid) -> Result<String, SignedURLError> {
-        let bucket = user_id.to_string();
-        let object = file_id.to_string();
+        let bucket = String::from("crispy-garbanzo");
+        let object = String::from(user_id.to_string() + "/" + &file_id.to_string());
         let url = self.client
             .signed_url(&bucket, &object, None, None, SignedURLOptions{
                 method: SignedURLMethod::PUT,
