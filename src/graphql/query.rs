8b4fd7d86dba9fc215b19414ff22139451e89f06
ltp1405 <lamtungphan1405@gmail.com>
2023-06-14 22:37:01 +0700

    feat(api): add basic user api definitions

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -1,6 +1,11 @@
 use async_graphql::*;
+use mongodb::bson::Uuid;
 
-use crate::{services::temp::temp_function, data_source::mongo::MongoDB};
+use crate::{
+    data_source::mongo::MongoDB,
+    models::{cv::CV, users::User},
+    services::temp::{temp_function, GqlResult},
+};
 
 pub struct Query;
 
@@ -9,5 +14,22 @@ impl Query {
     async fn placeholder_query(&self, ctx: &Context<'_>) -> Result<String> {
         let db = ctx.data_unchecked::<MongoDB>().db.clone();
         temp_function(db).await
+    }
+
+    async fn user_login(&self, ctx: &Context<'_>) -> GqlResult<String> {
+        todo!()
+    }
+
+    // NOTE: Using graphql paging maybe good in this case
+    async fn get_user_cvs(&self, ctx: &Context<'_>, user_id: Uuid) -> GqlResult<Vec<CV>> {
+        todo!()
+    }
+
+    async fn get_user_by_username(&self, ctx: &Context<'_>, username: String) -> GqlResult<User> {
+        todo!()
+    }
+
+    async fn get_user_photo(&self, ctx: &Context<'_>) -> GqlResult<String> {
+        todo!()
     }
 }
