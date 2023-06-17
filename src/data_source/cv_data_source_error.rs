cebb09f00f41e0391c67f93911a11657b0864628
produy <nguyentrinhduy2003@gmail.com>
2023-06-17 23:09:56 +0700

    feat(repo): add new CVDataSourceError

--- src/data_source/cv_data_source_error.rs
+++ src/data_source/cv_data_source_error.rs
@@ -7,6 +7,9 @@ pub enum CVDataSourceError {
     // uuid cannot be found
     UuidNotFound(Uuid),
 
+    // author id cannot be found
+    AuthorIdNotFound(Uuid),
+
     // description is longer than limit
     TooLongDescription,
 
@@ -49,6 +52,9 @@ impl fmt::Display for CVDataSourceError {
             }
             CVDataSourceError::TooLongTitle => {
                 write!(f, "Title is too long")
+            }
+            CVDataSourceError::AuthorIdNotFound(uuid) => {
+                write!(f, "Author id {:?} is not found", uuid)
             }
         }
     }
