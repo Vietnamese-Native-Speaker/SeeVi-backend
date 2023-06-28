f6dc52ea05f570bc74dbcf71e78ad1aeca0d52b3
zrefalogia <vuongquocphongv@gmail.com>
2023-06-28 21:31:43 +0700

    test(models): add test for user and cv

--- src/models/cv/tests.rs
+++ src/models/cv/tests.rs
@@ -48,3 +48,23 @@ fn test_create_cv_input_tag() {
         .unwrap();
     assert_eq!(test_cv_input.tags, vec!["tag".to_string()]);
 }
+
+#[test] 
+fn test_cv_from_input() {
+    use mongodb::bson::Uuid;
+    use crate::models::cv::cv::CV;
+    let uuid = Uuid::new();
+    let test_cv_input = CreateCVInputBuilder::default()
+        .with_author_id(uuid)
+        .with_title("title")
+        .with_tag("tag".to_string())
+        .build()
+        .unwrap();
+    let test_cv = CV::from(test_cv_input);
+    assert_eq!(test_cv.author_id, uuid);
+    assert_eq!(test_cv.title, "title".to_string());
+    assert_eq!(test_cv.tags, vec!["tag".to_string()]);
+    assert_eq!(test_cv.description, None);
+    assert_eq!(test_cv.comments, Vec::default());
+    assert_eq!(test_cv.cv, None);
+}
