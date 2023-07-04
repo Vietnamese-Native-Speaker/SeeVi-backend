52c0c254bee839c461f789bbc4933ce9a88fdcb4
produy <nguyentrinhduy2003@gmail.com>
2023-07-04 12:16:47 +0700

    test(repo): UpdateCVInput test

--- src/models/cv/tests.rs
+++ src/models/cv/tests.rs
@@ -1,4 +1,9 @@
-use crate::models::cv::create_cv_input::CreateCVInputBuilder;
+use mongodb::bson::Uuid;
+
+use crate::models::cv::{create_cv_input::CreateCVInputBuilder};
+
+use super::{CreateCVInput, update_cv_input::UpdateCVInputBuilder};
+
 #[test]
 fn test_create_cv_input_author_id() {
     use mongodb::bson::Uuid;
@@ -47,4 +52,23 @@ fn test_create_cv_input_tag() {
         .build()
         .unwrap();
     assert_eq!(test_cv_input.tags, vec!["tag".to_string()]);
+}
+
+#[test]
+fn test_update_cv() {
+    let id = Uuid::new();
+    let author_id = Uuid::new();
+    let test_cv_update = UpdateCVInputBuilder::default()
+        .with__id(id)
+        .with_author_id(author_id)
+        .with_description("description".to_string())
+        .with_tags(vec!["tag1".to_string(), "tag2".to_string()])
+        .with_title("title".to_string())
+        .build()
+        .unwrap();
+    assert_eq!(test_cv_update._id, id);
+    assert_eq!(test_cv_update.author_id, author_id);
+    assert_eq!(test_cv_update.description, Some("description".to_string()));
+    assert_eq!(test_cv_update.tags, Some(vec!["tag1".to_string(), "tag2".to_string()]));
+    assert_eq!(test_cv_update.title, Some("title".to_string()));
 }
