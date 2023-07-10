40f22f53034608443a49b3a2f5850ea5f2492358
zrefalogia <vuongquocphongv@gmail.com>
2023-07-10 14:37:26 +0700

    test(service): refresh token handling in integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,6 +8,7 @@ use crate::common::{
     default_route, make_login_request, make_register_request, print_json, user_detail,
 };
 
+#[serial_test::serial]
 #[tokio::test]
 async fn register_and_login() {
     pretty_env_logger::init();
@@ -21,24 +22,34 @@ async fn register_and_login() {
     let routes = default_route(schema);
 
     make_register_request("ltp", "ltp", &routes).await;
-    let token = make_login_request("ltp", "ltp", &routes).await;
-    print_json(&token);
-    let token = token
+    let login_result = make_login_request("ltp", "ltp", &routes).await;
+    let access_token = login_result
         .get("data")
         .unwrap()
-        .get("login")
+        .get("access")
         .unwrap()
         .as_str()
         .unwrap()
         .to_string();
-    let user_rs = user_detail(token, &routes).await;
+    
+    let refresh_token = login_result
+        .get("data")
+        .unwrap()
+        .get("refresh")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_string();
+    print_json(&refresh_token);
+    
+    let user_rs = user_detail(access_token, &routes).await;
     print_json(&user_rs);
     let user_rs = user_rs.get("data").unwrap().get("userDetail").unwrap();
     assert_eq!(user_rs.get("username").unwrap().as_str().unwrap(), "ltp");
 
-    let token = make_login_request("ltp", "ltp1405", &routes).await;
-    print_json(&token);
-    token
+    let login_result = make_login_request("ltp", "ltp1405", &routes).await;
+    print_json(&login_result);
+    login_result
         .get("errors")
         .expect("should have error due to wrong password");
 }
