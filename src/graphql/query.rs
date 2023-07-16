cd0cf533024ceb8cd2cb6825ce3e2f6bd072bb16
ltp1405 <lamtungphan1405@gmail.com>
2023-07-16 11:06:44 +0700

    refactor(graphql): update according to service change

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -3,7 +3,7 @@ use async_graphql::*;
 use crate::{
     data_source::mongo::MongoDB,
     models::users::User,
-    services::{user_service::UserService, auth_service::AuthService},
+    services::{auth_service::AuthService, user_service::UserService},
 };
 
 pub struct Query;
@@ -14,15 +14,24 @@ struct LoginInfo {
     password: String,
 }
 
+#[derive(SimpleObject)]
+struct LoginResult {
+    access_token: String,
+    refresh_token: String,
+}
+
 #[Object]
 impl Query {
-    async fn login(&self, ctx: &Context<'_>, login_info: LoginInfo) -> Result<String> {
+    async fn login(&self, ctx: &Context<'_>, login_info: LoginInfo) -> Result<LoginResult> {
         let db = ctx.data_unchecked::<MongoDB>();
         let rs =
             AuthService::authenticate(db, Some(login_info.username), None, login_info.password)
                 .await;
         match rs {
-            Ok(token) => Ok(token),
+            Ok(token) => Ok(LoginResult {
+                access_token: token.0,
+                refresh_token: token.1,
+            }),
             Err(e) => Err(e.into()),
         }
     }
@@ -34,7 +43,7 @@ impl Query {
             Some(token) => token,
             None => return Err("No token provided".into()),
         };
-        let rs = AuthService::decode_token(token);
+        let rs = AuthService::decode_token(token, true);
         let claims = match rs {
             Some(claims) => claims,
             None => return Err("Invalid token".into()),
