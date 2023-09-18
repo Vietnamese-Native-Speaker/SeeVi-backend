9e5f2100ce076dd8d6e4a2d6b02b700b44680492
PhucChuHoang <phucchu390@gmail.com>
2023-09-18 16:22:43 +0700

    feat(service): send, accept, reject friend request

--- src/models/friend_request.rs
+++ src/models/friend_request.rs
@@ -16,3 +16,25 @@ pub struct FriendRequest {
     pub created_at: bson::DateTime,
     pub updated_at: bson::DateTime,
 }
+
+impl FriendRequest {
+    pub fn new(from: bson::Uuid, to: bson::Uuid) -> Self {
+        Self {
+            from,
+            to,
+            status: FriendRequestStatus::Pending,
+            created_at: bson::DateTime::now(),
+            updated_at: bson::DateTime::now(),
+        }
+    }
+
+    pub fn accept(&mut self) {
+        self.status = FriendRequestStatus::Accepted;
+        self.updated_at = bson::DateTime::now();
+    }
+
+    pub fn reject(&mut self) {
+        self.status = FriendRequestStatus::Rejected;
+        self.updated_at = bson::DateTime::now();
+    }
+}
