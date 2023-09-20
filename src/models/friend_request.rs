57f0a93e43cca6b3e710af6774ffe30682e9af48
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-09-20 19:34:22 +0700

    feat(models): update model to use ObjectId

--- src/models/friend_request.rs
+++ src/models/friend_request.rs
@@ -1,4 +1,4 @@
-use mongodb::bson;
+use mongodb::bson::{self, oid::ObjectId};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
@@ -10,31 +10,36 @@ pub enum FriendRequestStatus {
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct FriendRequest {
-    pub from: bson::Uuid,
-    pub to: bson::Uuid,
+    pub from: ObjectId,
+    pub to: ObjectId,
+    pub message: String,
     pub status: FriendRequestStatus,
     pub created_at: bson::DateTime,
     pub updated_at: bson::DateTime,
 }
 
 impl FriendRequest {
-    pub fn new(from: bson::Uuid, to: bson::Uuid) -> Self {
+    pub fn new(from: ObjectId, to: ObjectId, message: impl Into<String>) -> Self {
         Self {
             from,
             to,
+            message: message.into(),
             status: FriendRequestStatus::Pending,
             created_at: bson::DateTime::now(),
             updated_at: bson::DateTime::now(),
         }
     }
 
-    pub fn accept(&mut self) {
+    pub fn accept(mut self) -> Self {
         self.status = FriendRequestStatus::Accepted;
         self.updated_at = bson::DateTime::now();
+        self
     }
 
-    pub fn reject(&mut self) {
+    pub fn reject(mut self) -> Self {
         self.status = FriendRequestStatus::Rejected;
         self.updated_at = bson::DateTime::now();
+        self
     }
 }
+
