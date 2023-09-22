deb961fdb420401964ee1ab28e535acc855c6e33
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-09-22 12:43:59 +0700

    test(service): friend list related tests

--- src/services/user_service/tests.rs
+++ src/services/user_service/tests.rs
@@ -1,3 +1,5 @@
+use async_graphql::futures_util::StreamExt;
+
 use crate::{
     models::users::{CreateUserInput, UpdateUserInput},
     services::tests::MockDatabase,
@@ -39,4 +41,88 @@ async fn test_update_user() {
     assert_eq!("test", user.username);
     assert_eq!("test2", user.password);
     assert_eq!("test2@gmail.com", user.primary_email);
+}
+
+#[tokio::test]
+async fn test_send_then_accept_friend_request() {
+    let db = MockDatabase::new();
+    let input = mock_user_input();
+    let rs = UserService::create_user(&db, input).await.unwrap();
+    let input2 = mock_user_input();
+    let rs2 = UserService::create_user(&db, input2).await.unwrap();
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+    UserService::send_friend_request(&db, *rs.id, *rs2.id, Some("hello"))
+        .await
+        .unwrap();
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+
+    UserService::accept_friend_request(&db, *rs.id, *rs2.id)
+        .await
+        .unwrap_err();
+
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+
+    UserService::accept_friend_request(&db, *rs2.id, *rs.id)
+        .await
+        .unwrap();
+
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(1, friends_list.len());
+}
+
+#[tokio::test]
+async fn test_send_then_decline_friend_request() {
+    let db = MockDatabase::new();
+    let input = mock_user_input();
+    let rs = UserService::create_user(&db, input).await.unwrap();
+    let input2 = mock_user_input();
+    let rs2 = UserService::create_user(&db, input2).await.unwrap();
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+    UserService::send_friend_request(&db, *rs.id, *rs2.id, Some("hello"))
+        .await
+        .unwrap();
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+
+    UserService::reject_friend_request(&db, *rs.id, *rs2.id)
+        .await
+        .unwrap_err();
+
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
+
+    UserService::reject_friend_request(&db, *rs2.id, *rs.id)
+        .await
+        .unwrap();
+
+    let friends_list = UserService::friend_lists(&db, *rs.id)
+        .await
+        .collect::<Vec<_>>()
+        .await;
+    assert_eq!(0, friends_list.len());
 }
