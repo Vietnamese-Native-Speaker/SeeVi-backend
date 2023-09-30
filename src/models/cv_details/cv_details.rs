c760e84f51e0341a8441ae01362d075b6d50f0ac
produy <nguyentrinhduy2003@gmail.com>
2023-09-30 19:06:09 +0700

    fix(repos): fix struct CVDetails

--- src/models/cv_details/cv_details.rs
+++ src/models/cv_details/cv_details.rs
@@ -1,25 +1,9 @@
 use async_graphql::{ComplexObject, SimpleObject};
 use mongodb::bson::{self, DateTime, Uuid};
 use serde::{Deserialize, Serialize};
+use crate::models::{sex::Sex, range_values::RangeValues};
 
-use crate::{models::ResourceIdentifier, object_id::ScalarObjectId};
-use super::{CreateCVInput, Sex};
-
-/// Struct represents CV defined in the Diagram. Note that this struct only
-/// represents the metadata of a CV. Actual implementation of the CV is to
-/// be discussed.
-#[derive(Debug, Serialize, Deserialize, Clone, SimpleObject, PartialEq)]
-#[graphql(complex)]
-
-pub struct RangeValues{
-    pub lower: int,
-    pub upper: int
-}
-pub enum Sex{
-    Female,
-    Male,
-    Others(String),
-}
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct CVDetails {
     pub country: Option<String>,
     pub city: Option<String>,
@@ -29,11 +13,4 @@ pub struct CVDetails {
     pub search_words: Vec<String>,
     pub range_values: Option<RangeValues>,
     pub sex: Option<Sex>,
-}
-
-#[ComplexObject]
-impl CVDetails {
-    async fn created(&self) -> String {
-        self.created.try_to_rfc3339_string().unwrap()
-    }
 }
