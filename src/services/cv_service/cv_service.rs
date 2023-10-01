cb72ba1a3b0b7c7559655db772ec45ce401079ef
zrefalogia <vuongquocphongv@gmail.com>
2023-10-01 10:58:36 +0700

    fix(service): change variables order

--- src/services/cv_service/cv_service.rs
+++ src/services/cv_service/cv_service.rs
@@ -2,16 +2,14 @@ use mongodb::bson::oid::ObjectId;
 
 use crate::data_source::{CVDataSource, CVDataSourceError};
 use crate::models::cv::{UpdateCVInput, CV};
-use std::boxed::Box;
-use std::fmt::Debug;
 
 pub struct CVService {}
 
 impl CVService {
     pub async fn change_title(
-        title: String,
-        cv_id: ObjectId,
         database: &(impl CVDataSource + std::marker::Sync),
+        cv_id: ObjectId,
+        title: String
     ) -> Result<CV, CVDataSourceError> {
         let cv = database.get_cv_by_id(cv_id).await;
         match cv {
@@ -59,7 +57,8 @@ impl CVService {
             }
         }
     }
-    pub async fn add_tags(
+
+    pub async fn add_tag(
         database: &(impl CVDataSource + std::marker::Sync),
         cv_id: ObjectId,
         tag: String,
@@ -68,7 +67,7 @@ impl CVService {
         match cv {
             Ok(cv) => {
                 let mut tags = cv.clone().tags;
-                tags.push(tag);
+                tags.push(tag.clone());
                 let input: UpdateCVInput = UpdateCVInput {
                     id: cv_id.into(),
                     author_id: cv.author_id.into(),
@@ -84,7 +83,36 @@ impl CVService {
             }
         }
     }
-    pub async fn remove_tags(
+
+    pub async fn add_tags(
+        database: &(impl CVDataSource + std::marker::Sync),
+        cv_id: ObjectId,
+        to_add_tags: Vec<String>,
+    ) -> Result<CV, CVDataSourceError> {
+        let cv = database.get_cv_by_id(cv_id).await;
+        match cv {
+            Ok(cv) => {
+                let mut tags = cv.clone().tags;
+                for tag in to_add_tags {
+                    tags.push(tag.clone());
+                }
+                let input: UpdateCVInput = UpdateCVInput {
+                    id: cv_id.into(),
+                    author_id: cv.author_id.into(),
+                    title: None,
+                    description: None,
+                    tags: Some(tags),
+                };
+                let rs = database.update_cv_info(input).await;
+                return Ok(rs?);
+            }
+            Err(_) => {
+                return Err(CVDataSourceError::IdNotFound(cv_id));
+            }
+        }
+    }
+
+    pub async fn remove_tag(
         database: &(impl CVDataSource + std::marker::Sync),
         cv_id: ObjectId,
         tag: String,
@@ -94,6 +122,36 @@ impl CVService {
             Ok(cv) => {
                 let mut tags = cv.clone().tags;
                 tags.remove(tags.iter().position(|x| *x == tag).unwrap());
+                let input: UpdateCVInput = UpdateCVInput {
+                    id: cv_id.into(),
+                    author_id: cv.author_id.into(),
+                    title: None,
+                    description: None,
+                    tags: Some(tags),
+                };
+                let rs = database.update_cv_info(input).await;
+                return Ok(rs?);
+            }
+            Err(_) => {
+                return Err(CVDataSourceError::IdNotFound(cv_id));
+            }
+        }
+    }
+
+    pub async fn remove_tags(
+        database: &(impl CVDataSource + std::marker::Sync),
+        cv_id: ObjectId,
+        to_del_tags: Vec<String>,
+    ) -> Result<CV, CVDataSourceError> {
+        let cv = database.get_cv_by_id(cv_id).await;
+        match cv {
+            Ok(cv) => {
+                let mut tags = cv.clone().tags;
+                for tag in to_del_tags {
+                    if tags.iter().position(|x| *x == tag).is_some() {
+                        tags.remove(tags.iter().position(|x| *x == tag).unwrap());
+                    }
+                }
                 let input: UpdateCVInput = UpdateCVInput {
                     id: cv_id.into(),
                     author_id: cv.author_id.into(),
