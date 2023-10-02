8073b3d12c68c298b7fbb2a13b47b0ee02ecabb6
zrefalogia <vuongquocphongv@gmail.com>
2023-10-02 11:35:28 +0700

    perf(data): remove unimplemented

--- src/data_source/comment/mod.rs
+++ src/data_source/comment/mod.rs
@@ -1,39 +1,29 @@
 mod error;
 
-use async_trait::async_trait;
-use mongodb::bson::{self, Uuid};
 use async_graphql::futures_util::stream::BoxStream;
+use async_trait::async_trait;
+use google_cloud_storage::http::Error;
+use mongodb::bson::{self, Uuid};
 
 pub use error::CommentDataSourceError;
 
-use crate::models::{
-    comment::{Comment, CreateCommentInput, UpdateCommentInput},
-    ResourceIdentifier,
-};
+use crate::models::comment::{Comment, CreateCommentInput, UpdateCommentInput};
 
 #[async_trait]
-pub trait CommentDataSource{
-    async fn get_comment_by_id(&self, _id: bson::oid::ObjectId) -> Result<Comment, CommentDataSourceError> {
-        unimplemented!()
-    }
+pub trait CommentDataSource {
+    type Error;
+    async fn get_comment_by_id(&self, _id: bson::oid::ObjectId) -> Result<Comment, Error>;
 
-    async fn get_comments_by_cv_id(&self, _cv_id: bson::oid::ObjectId) -> BoxStream<Result<Comment, CommentDataSourceError>> {
-        unimplemented!()
-    }
+    async fn get_comments_by_cv_id(
+        &self,
+        _cv_id: bson::oid::ObjectId,
+    ) -> BoxStream<Result<Comment, Error>>;
 
-    async fn create_comment(&self, _input: CreateCommentInput) -> Result<Comment, CommentDataSourceError> {
-        unimplemented!()
-    }
+    async fn create_comment(&self, _input: CreateCommentInput) -> Result<Comment, Error>;
 
-    async fn add_comment(&self, _comment: Comment) -> Result<(), CommentDataSourceError> {
-        unimplemented!()
-    }
+    async fn add_comment(&self, _comment: Comment) -> Result<(), Error>;
 
-    async fn update_comment(&self, _input: UpdateCommentInput) -> Result<Comment, CommentDataSourceError> {
-        unimplemented!()
-    }
+    async fn update_comment(&self, _input: UpdateCommentInput) -> Result<Comment, Error>;
 
-    async fn delete_comment(&self, _id: bson::oid::ObjectId) -> Result<Comment, CommentDataSourceError> {
-        unimplemented!()
-    }
+    async fn delete_comment(&self, _id: bson::oid::ObjectId) -> Result<Comment, Error>;
 }
