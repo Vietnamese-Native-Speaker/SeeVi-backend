cb5037b3a33d2b119812fb0594491f9ec47148bf
produy <nguyentrinhduy2003@gmail.com>
2023-10-03 23:01:49 +0700

    test(repos): add test for queries of filter

--- src/data_source/tests/test_mongo_cv_data_source.rs
+++ src/data_source/tests/test_mongo_cv_data_source.rs
@@ -1,13 +1,18 @@
 use super::super::{cv_data_source::CVDataSource, cv_data_source_error::CVDataSourceError};
+use crate::data_source::cv_details_data_source::CVDetailsDataSource;
 use crate::data_source::mongo::MongoDB;
 use crate::data_source::user_data_source::UserDataSource;
 use crate::models::cv::create_cv_input::CreateCVInputBuilder;
 use crate::models::cv::CreateCVInput;
-use crate::models::education::Education;
+use crate::models::cv_details::CVDetails;
+use crate::models::cv_details::cv_details::CVDetailsBuilder;
+use crate::models::{education::Education, sex::Sex, range_values::RangeValues};
 use crate::models::users::create_user_input::CreateUserInputBuilder;
 use crate::models::users::CreateUserInput;
 use mongodb::bson::Uuid;
 use serial_test::serial;
+use futures_core::stream::BoxStream;
+use tokio_stream::StreamExt;
 
 fn create_demo_user_input(test_uuid: Uuid) -> CreateUserInput {
     CreateUserInputBuilder::default()
@@ -33,6 +38,14 @@ fn create_demo_user_input(test_uuid: Uuid) -> CreateUserInput {
         .with_about("about".to_string())
         .with_avatar(test_uuid)
         .with_cover_photo(test_uuid)
+        .with_country("country")
+        .with_city("city")
+        .with_major("major")
+        .with_personalities("personality")
+        .with_rating(4.0)
+        .with_search_words("search_words")
+        .with_sex(Sex::Male)
+        .with_year_of_experience("year_of_experience")
         .build()
         .unwrap()
 }
@@ -48,6 +61,19 @@ fn create_demo_cv_input(test_uuid: Uuid) -> CreateCVInput {
         .unwrap()
 }
 
+fn create_demo_cv_details() -> CVDetails{
+    CVDetailsBuilder::default()
+        .with_country("country")
+        .with_city("city")
+        .with_major("major")
+        .with_personalities("personality")
+        .with_rating(RangeValues{ upper: 5.0, lower: 0.0})
+        .with_search_words("search_words")
+        .with_sex(Sex::Male)
+        .with_year_of_experience("year_of_experience")
+        .build()
+        .unwrap()
+}
 #[tokio::test]
 #[serial]
 async fn test_create_cv() {
@@ -94,3 +120,19 @@ async fn get_cv_by_id() {
 // fn test_delete_cv(){
 
 // }
+
+#[tokio::test]
+#[serial]
+async fn test_get_cvs_by_filter() {
+    let mongodb = MongoDB::init_test().await;
+    let uuid = Uuid::new();
+    let user = create_demo_user_input(uuid);
+    let input_user = mongodb.create_user(user).await.unwrap();
+    let input = create_demo_cv_input(input_user.user_id);
+    let check_input = mongodb.create_cv(input).await.unwrap();
+    let cv_filter = create_demo_cv_details();
+    let mut stream_cv = mongodb.get_cvs_by_filter(cv_filter).await;
+    while let Some(cv) = stream_cv.next().await {
+        assert_eq!(cv.unwrap().tags, vec!["tag".to_string(), "tag2".to_string()]);
+    }
+}
