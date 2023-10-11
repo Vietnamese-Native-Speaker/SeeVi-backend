d7ea3af9f4d7c9cdfd829f49d935b300988a57b4
produy <nguyentrinhduy2003@gmail.com>
2023-10-11 11:17:50 +0700

    feat(repos): add datasourceerror for cvdetails

--- src/data_source/cv_details_data_source.rs
+++ src/data_source/cv_details_data_source.rs
@@ -1,17 +1,16 @@
 use async_graphql::async_trait::async_trait;
 use mongodb::bson::Uuid;
-
+use std::pin::Pin;
 use crate::models::cv::{CreateCVInput, CV};
 use crate::models::cv_details::{CVDetails};
-use super::cv_data_source_error::CVDataSourceError;
+use super::cv_details_data_source_error::CVDetailsDataSourceError;
 use futures_core::stream::BoxStream;
+use tokio_stream::Stream;
 
-/// Primary abstraction for CV Data Source. Ones should implement this trait for
-/// different type of database in order to provide that data source to services
 #[async_trait]
 pub trait CVDetailsDataSource {
-    /// Return the CV with the provided id.
-    async fn get_cvs_by_filter(&self, cv_details: CVDetails) -> BoxStream<Result<CV, CVDataSourceError>> {
+    /// Return the CV with the provided filter.
+    async fn get_cvs_by_filter(&self, cv_details: CVDetails) -> Result<Pin<Box<dyn Stream<Item = CV>>>, CVDetailsDataSourceError> {
         unimplemented!()
     }
 }
--- src/data_source/mod.rs
+++ src/data_source/mod.rs
@@ -4,6 +4,7 @@ pub mod mongo;
 pub mod user_data_source;
 pub mod user_data_source_error;
 pub mod cv_details_data_source;
+pub mod cv_details_data_source_error;
 
 #[cfg(test)]
 mod tests;
