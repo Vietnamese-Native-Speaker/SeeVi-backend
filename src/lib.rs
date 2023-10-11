efe0bc62f7296b02c6a7705c5c8bdf634a2cabd2
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-10-11 12:33:57 +0700

    feat(api): add cmdline argument for running in test mode

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,73 @@ extern crate derive_builder;
 use std::convert::Infallible;
 
 use async_graphql::{EmptySubscription, Schema};
-use async_graphql_warp::{GraphQLResponse, GraphQLBadRequest};
-use data_source::mongo;
-use filters::{with_auth_header, graphql_sdl};
+use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
+use data_source::mongo::{self, MongoForTesting};
+use filters::{graphql_sdl, with_auth_header};
 use graphql::{mutation::Mutation, query::Query};
-use warp::{Filter, Rejection, hyper::StatusCode};
+use mock_data_generation::generate_mock_data;
+use warp::{hyper::StatusCode, Filter, Rejection};
 
-pub mod object_id;
 pub mod data_source;
-pub mod graphql;
-pub mod models;
-pub mod services;
-pub mod filters;
 pub mod error;
+pub mod filters;
+pub mod graphql;
 pub mod mock_data_generation;
+pub mod models;
+pub mod object_id;
+pub mod services;
 
 #[derive(Clone)]
 pub struct State {
     pub schema: Schema<Query, Mutation, EmptySubscription>,
+}
+
+pub async fn run_server_for_test() {
+    pretty_env_logger::init();
+
+    let mongo_ds = MongoForTesting::init().await;
+    generate_mock_data(&mongo_ds).await;
+
+    let schema = Schema::build(Query, Mutation, EmptySubscription)
+        .data(mongo_ds)
+        .finish();
+
+    let graphql_post = with_auth_header()
+        .and(async_graphql_warp::graphql(schema.clone()))
+        .and_then(
+            |header,
+             (schema, request): (
+                Schema<Query, Mutation, EmptySubscription>,
+                async_graphql::Request,
+            )| async move {
+                Ok::<_, Rejection>(GraphQLResponse::from(
+                    schema.execute(request.data(header)).await,
+                ))
+            },
+        );
+    let logger = warp::log("seevi_backend");
+
+    let routes = warp::path!("graphql" / "playground")
+        .and(filters::graphql_playground())
+        .with(logger)
+        .or(warp::path!("graphql" / "schema").and(graphql_sdl(schema)))
+        .with(logger)
+        .or(warp::path!("graphql").and(graphql_post))
+        .with(logger)
+        .recover(|err: Rejection| async move {
+            if let Some(GraphQLBadRequest(err)) = err.find() {
+                return Ok::<_, Infallible>(warp::reply::with_status(
+                    err.to_string(),
+                    StatusCode::BAD_REQUEST,
+                ));
+            }
+
+            Ok(warp::reply::with_status(
+                "INTERNAL_SERVER_ERROR".to_string(),
+                StatusCode::INTERNAL_SERVER_ERROR,
+            ))
+        });
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
 
 pub async fn run_server() {
