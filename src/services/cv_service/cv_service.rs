cd93ba7d5eac3542af9287e1120207ed58d44ee8
produy <nguyentrinhduy2003@gmail.com>
2023-10-11 11:33:55 +0700

    feat(service): add find_suggested_cvs

--- src/services/cv_service/cv_service.rs
+++ src/services/cv_service/cv_service.rs
@@ -1,7 +1,15 @@
-use crate::data_source::cv_data_source_error::CVDataSourceError;
+use crate::data_source::{
+    cv_data_source_error::CVDataSourceError, 
+    cv_data_source::CVDataSource,
+    cv_details_data_source::CVDetailsDataSource,
+    cv_details_data_source_error::CVDetailsDataSourceError};
 use crate::models::cv::CV;
+use crate::models::cv_details::CVDetails;
 use std::boxed::Box;
 use std::fmt::Debug;
+use futures_core::stream::BoxStream;
+use std::pin::Pin;
+use tokio_stream::Stream;
 
 pub struct CVService {}
 
@@ -26,7 +34,7 @@ impl CVService {
     }
 
     // NOTE: The return type should `Stream<Item = CV>`
-    pub async fn find_suggested_cvs(&self) -> Result<Vec<CV>, CVDataSourceError> {
-        todo!()
+    pub async fn find_suggested_cvs<'a>(&'a self, database: &'a(impl CVDetailsDataSource + std::marker::Sync), cv_details: CVDetails) -> Result<Pin<Box<dyn Stream<Item = CV>>>, CVDetailsDataSourceError> {
+        database.get_cvs_by_filter(cv_details).await
     }
 }
