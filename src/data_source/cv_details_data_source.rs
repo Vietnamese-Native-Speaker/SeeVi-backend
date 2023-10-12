a05d956020975747886bb8dd4d29750837e12eba
produy <nguyentrinhduy2003@gmail.com>
2023-10-12 22:45:55 +0700

    feat(repos): add associated type Error forCVDetail

--- src/data_source/cv_details_data_source.rs
+++ src/data_source/cv_details_data_source.rs
@@ -3,6 +3,7 @@ use mongodb::bson::Uuid;
 
 use crate::models::cv::{CreateCVInput, CV};
 use crate::models::cv_details::{CVDetails};
+use crate::services::cv_service::error::CVServiceError;
 use super::cv_data_source_error::CVDataSourceError;
 use futures_core::stream::BoxStream;
 
@@ -10,8 +11,9 @@ use futures_core::stream::BoxStream;
 /// different type of database in order to provide that data source to services
 #[async_trait]
 pub trait CVDetailsDataSource {
-    /// Return the CV with the provided id.
-    async fn get_cvs_by_filter(&self, cv_details: CVDetails) -> BoxStream<Result<CV, CVDataSourceError>> {
+    /// Return the CV with the provided filter.
+    type Error: std::error::Error + Sync + Send + Into<CVServiceError>;
+    async fn get_cvs_by_filter(&self, cv_details: CVDetails) -> Result<Pin<Box<dyn Stream<Item = CV>>>, Self::Error> {
         unimplemented!()
     }
 }
