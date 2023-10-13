7f192dc3208bdf606bc817ed7b9936521118e232
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-10-14 00:54:59 +0700

    fix(repo): change to BoxStream for more correct typing

--- src/data_source/cv_details/mod.rs
+++ src/data_source/cv_details/mod.rs
@@ -1,20 +1,21 @@
 use std::pin::Pin;
 
 use async_graphql::async_trait::async_trait;
-use mongodb::bson::Uuid;
-
-use crate::models::cv::{CreateCVInput, CV};
-use crate::models::cv_details::{CVDetails};
-use crate::services::cv_service::error::CVServiceError;
 use futures_core::stream::BoxStream;
+
+use crate::models::cv::CV;
+use crate::models::cv_details::CVDetails;
+use crate::services::cv_service::error::CVServiceError;
 use tokio_stream::Stream;
-/// Primary abstraction for CV Data Source. Ones should implement this trait for
+
+/// Primary abstraction for CV Details Data Source. Ones should implement this trait for
 /// different type of database in order to provide that data source to services
 #[async_trait]
 pub trait CVDetailsDataSource {
     /// Return the CV with the provided filter.
     type Error: std::error::Error + Sync + Send + Into<CVServiceError>;
-    async fn get_cvs_by_filter(&self, cv_details: CVDetails) -> Result<Pin<Box<dyn Stream<Item = CV>>>, Self::Error> {
-        unimplemented!()
-    }
+    async fn get_cvs_by_filter(
+        &self,
+        cv_details: CVDetails,
+    ) -> Result<BoxStream<CV>, Self::Error>;
 }
