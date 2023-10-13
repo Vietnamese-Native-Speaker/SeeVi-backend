b3bc90f5e6322c3b47d747e3acaf64efe5f33ba5
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-10-14 00:58:25 +0700

    feat(graphql): add graphql api

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -1,3 +1,5 @@
+use std::pin::Pin;
+
 use async_graphql as gql;
 use async_graphql::{futures_util::StreamExt, Context, InputObject, Object};
 use gql::{connection, ErrorExtensions};
@@ -5,7 +7,10 @@ use mongodb::bson::oid::ObjectId;
 use mongodb::options::AuthMechanism;
 
 use crate::error::ServerError;
+use crate::models::cv::CV;
+use crate::models::cv_details::CVDetails;
 use crate::object_id::ScalarObjectId;
+use crate::services::cv_service::cv_service::CVService;
 use crate::{
     data_source::mongo::{MongoDB, MongoForTesting},
     models::users::User,
@@ -135,6 +140,76 @@ impl Query {
                     .extend(friends_list.into_iter().map(|friend| {
                         connection::Edge::new(friend.as_ref().unwrap().id, friend.unwrap())
                     }));
+                Ok::<_, async_graphql::Error>(connection)
+            },
+        )
+        .await
+    }
+
+    async fn cvs_list(
+        &self,
+        ctx: &Context<'_>,
+        filter: CVDetails,
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> gql::Result<
+        connection::Connection<
+            ScalarObjectId,
+            CV,
+            connection::EmptyFields,
+            connection::EmptyFields,
+        >,
+    > {
+        let db = ctx
+            .data_opt::<MongoDB>()
+            .unwrap_or_else(|| ctx.data_unchecked::<MongoForTesting>());
+        authorization(ctx)?;
+        let stream = CVService::find_suggested_cvs(db, filter).await?;
+        let stream = Pin::from(Box::new(stream)).collect::<Vec<_>>().await;
+        connection::query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                let friends_list = if let Some(after) = after {
+                    stream
+                        .into_iter()
+                        .skip_while(|cv| cv.id != after)
+                        .skip(1)
+                        .map(|friend| friend)
+                        .collect::<Vec<_>>()
+                } else if let Some(before) = before {
+                    stream
+                        .into_iter()
+                        .take_while(|cv| cv.id != before)
+                        .map(|friend| friend)
+                        .collect::<Vec<_>>()
+                } else {
+                    stream.into_iter().collect::<Vec<_>>()
+                };
+                let friends_list = if let Some(first) = first {
+                    friends_list
+                        .into_iter()
+                        .take(first as usize)
+                        .collect::<Vec<_>>()
+                } else if let Some(last) = last {
+                    let size = friends_list.len() as usize;
+                    friends_list
+                        .into_iter()
+                        .skip(size - last as usize)
+                        .collect::<Vec<_>>()
+                } else {
+                    panic!("Must have either 'first' or 'last' argument")
+                };
+                let mut connection = connection::Connection::new(true, false);
+                connection.edges.extend(
+                    friends_list
+                        .into_iter()
+                        .map(|cv| connection::Edge::new(cv.id, cv)),
+                );
                 Ok::<_, async_graphql::Error>(connection)
             },
         )
