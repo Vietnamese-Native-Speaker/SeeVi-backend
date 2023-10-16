fca38857774018bed157ba7b8df8893fabb63ad2
ltp1405 <lamtungphan1405@gmail.com>
2023-10-16 21:57:47 +0700

    refactor(model): some minor refactoring

--- src/models/comment/mod.rs
+++ src/models/comment/mod.rs
@@ -1,8 +1,10 @@
-mod comment;
 mod bookmark;
+mod comment;
 mod create_comment_input;
 pub mod update_comment_input;
 
+pub use bookmark::Bookmark;
+pub use bookmark::Key as BookmarkKey;
 pub use comment::Comment;
 pub use create_comment_input::CreateCommentInput;
 pub use update_comment_input::UpdateCommentInput;
