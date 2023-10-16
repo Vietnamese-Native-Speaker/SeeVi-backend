135d7d4a1ebcdbcbc178b19eed0e5e3ab70cec58
ltp1405 <lamtungphan1405@gmail.com>
2023-10-16 22:02:54 +0700

    refactor(common): minor refactoring

--- src/models/comment/mod.rs
+++ src/models/comment/mod.rs
@@ -4,6 +4,7 @@ pub mod like;
 pub mod create_comment_input;
 
 pub use like::Like;
+pub use like::Key as LikeKey;
 pub use comment::Comment;
 pub use create_comment_input::CreateCommentInput;
 pub use update_comment_input::UpdateCommentInput;
