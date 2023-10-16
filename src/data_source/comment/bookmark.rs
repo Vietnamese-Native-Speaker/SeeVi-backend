f9683234e036613fbd3eab3cccfac3619c1e1826
ltp1405 <lamtungphan1405@gmail.com>
2023-10-16 22:31:52 +0700

    feat(repo): add datasource trait

---
+++ src/data_source/comment/bookmark.rs
@@ -0,0 +1,31 @@
+use async_graphql::futures_util::stream::BoxStream;
+use async_trait::async_trait;
+use mongodb::bson::oid::ObjectId;
+
+use crate::{
+    models::comment::Bookmark, services::cv_service::comment_service::CommentServiceError,
+};
+
+#[async_trait]
+pub trait BookmarkDataSource {
+    type Error: std::error::Error + Send + Sync + Into<CommentServiceError>;
+
+    async fn add_bookmark(
+        &self,
+        user_id: ObjectId,
+        comment_id: ObjectId,
+    ) -> Result<(), Self::Error>;
+
+    async fn delete_bookmark(
+        &self,
+        user_id: ObjectId,
+        comment_id: ObjectId,
+    ) -> Result<(), Self::Error>;
+
+    async fn get_bookmarks_of_user(
+        &self,
+        user_id: ObjectId,
+    ) -> Result<BoxStream<Result<Bookmark, Self::Error>>, Self::Error>;
+
+    async fn get_bookmark(&self, user_id: ObjectId, comment_id: ObjectId) -> Result<Option<Bookmark>, Self::Error>;
+}
