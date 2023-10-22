20ee97cb0ff775a04fc47702ecff92836f95da85
ltp1405 <lamtungphan1405@gmail.com>
2023-10-22 22:49:26 +0700

    feat(test): add macro for creating graphq queries

---
+++ graphql-query-maker/src/variable.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Variable {
+    pub name: String,
+    pub nullable: bool,
+}
+
+pub(crate) fn make_function_variables(variables: &[Variable]) -> String {
+    if variables.is_empty() {
+        return String::new();
+    }
+    let first = if variables[0].nullable {
+        format!(
+            "{}: Option<impl Into<serde_json::Value>>",
+            variables[0].name
+        )
+    } else {
+        format!("{}: impl Into<serde_json::Value>", variables[0].name)
+    };
+    variables.into_iter().skip(1).fold(first, |acc, var| {
+        if var.nullable {
+            format!(
+                "{}, {}: Option<impl Into<serde_json::Value>>",
+                acc, var.name
+            )
+        } else {
+            format!("{}, {}: impl Into<serde_json::Value>", acc, var.name)
+        }
+    })
+}
